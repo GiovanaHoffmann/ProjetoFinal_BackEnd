f04616caa9a66264dbb9e22761b3a11a771acb31
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-11-30 00:13:22 -0300

    conexão com mongoose

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+
 const port = 3001;
+
+const dotenv = require('dotenv').config;
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/estoque")
+.then(async () => {
+    console.log('Datbase conected!');
+  }).catch(error => {
+    console.error('Error connecting MongoDB:', error.message);
+  });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
