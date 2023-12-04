90f9d6f1d06bc90521ea12417792705102a54f2f
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-12-04 04:37:33 -0300

    .env esquecido

--- config/database.js
+++ config/database.js
@@ -2,6 +2,9 @@ const Sequelize = require('sequelize');
 const sequelize = new 
 Sequelize(process.env.MYSQL_LINK);
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const connectToDatabase = async () => {
     try {
       await sequelize.authenticate();
