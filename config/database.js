b42a04c3322c946c6f327646a9bd9cbca21cfc74
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-12-04 05:52:36 -0300

    conexao tirada de database.js

--- config/database.js
+++ config/database.js
@@ -1,22 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const Sequelize = require('sequelize');
 const sequelize = new 
 Sequelize(process.env.MYSQL_LINK);
 
-const dotenv = require('dotenv');
-dotenv.config();
 
-const connectToDatabase = async () => {
-    try {
-      await sequelize.authenticate();
-      console.log('MySQL connected.');
-      
-      await sequelize.query('CREATE DATABASE IF NOT EXISTS estoque;');
-      console.log('Database created or already exists.');
-    } catch (err) {
-      console.error('Error connecting to the database:', err);
-    }
-  };
-  
-  connectToDatabase();
   
   module.exports = sequelize;
