13883f7721c2feb2d93f8b451da6aafeb33d8a34
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-12-07 06:20:10 -0300

    rotas e sincronização dos modelos com BD

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+//configuração da aplicação e iniciação do servidor
 const express = require('express');
 const app = express();
 const sequelize = require('./config/database');
+const Usuario = require('./models/Usuario');
+const Funcionario = require('./models/funcionario');
+const Gerente = require('./models/gerente');
+const Produto = require('./models/produto');
 
-const dotenv = require('dotenv').config();
-const PORT = process.env.PORT;
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+console.log(process.env.PORT);
+console.log(process.env.MYSQL_LINK);
 
 // Configurações do Express, Middlewares e Body Parser
 app.use(express.json());
 
-//rotas
-app.use('/products', require('./routes/produtoRoutes'));
+// Rotas
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/usuarioRoutes');
+const productRoutes = require('./routes/produtoRoutes');
+const install = require('./routes/installRoute');
 
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/install', install);
 
+//Servidor e banco de dados
  sequelize.authenticate()
   .then(() => {
     console.log('MySQL connected.');
@@ -25,3 +43,12 @@ app.use('/products', require('./routes/produtoRoutes'));
   .catch(err => {
     console.error('Error connecting to the database:', err);
   });
+
+  // Sincronize os modelos com o banco de dados
+sequelize.sync({ force: true }) // O uso de force: true irá forçar a criação das tabelas, cuidado com dados existentes
+.then(() => {
+  console.log('Banco de dados sincronizado.');
+})
+.catch((err) => {
+  console.error('Erro ao sincronizar o banco de dados:', err);
+});
