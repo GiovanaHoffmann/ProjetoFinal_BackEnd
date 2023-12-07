c21001301e4dda9cf9bdbbda8b2aad5586297f26
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-12-07 06:16:22 -0300

    atualização da linha de instância

--- config/database.js
+++ config/database.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const Sequelize = require('sequelize');
-const sequelize = new 
-Sequelize(process.env.MYSQL_LINK);
+const sequelize = new Sequelize(process.env.MYSQL_LINK);
   
   module.exports = sequelize;
