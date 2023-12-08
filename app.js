efd3f8a10fe7aec234eb1da26418bf66ce3ab804
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-12-08 18:38:13 -0300

    atualização da conexão e rotas

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//configuração da aplicação e iniciação do servidor
 const express = require('express');
 const app = express();
 const sequelize = require('./config/database');
@@ -12,25 +11,51 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-console.log(process.env.PORT);
-console.log(process.env.MYSQL_LINK);
-
-// Configurações do Express, Middlewares e Body Parser
 app.use(express.json());
 
 // Rotas
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/usuarioRoutes');
+const funcionarioRoutes = require('./routes/funcionarioRoutes');
+const gerenteRoutes = require('./routes/gerenteRoutes');
 const productRoutes = require('./routes/produtoRoutes');
 const install = require('./routes/installRoute');
 
+
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/funcionarios', funcionarioRoutes);
+app.use('/gerentes', gerenteRoutes);
 app.use('/products', productRoutes);
 app.use('/install', install);
 
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('MySQL connected.');
+
+    // Sincronize os modelos com o banco de dados
+    sequelize.sync({ force: true }) // O uso de force: true irá forçar a criação das tabelas, cuidado com dados existentes
+      .then(() => {
+        console.log('Banco de dados sincronizado.');
+
+        // Inicie o servidor após a sincronização bem-sucedida
+        app.listen(PORT, () => {
+          console.log(`Server running on port ${PORT}`);
+        });
+      })
+      .catch((err) => {
+        console.error('Erro ao sincronizar o banco de dados:', err);
+      });
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+  });
+
+
 //Servidor e banco de dados
- sequelize.authenticate()
+ /*sequelize.authenticate()
   .then(() => {
     console.log('MySQL connected.');
     
@@ -44,11 +69,11 @@ app.use('/install', install);
     console.error('Error connecting to the database:', err);
   });
 
-  // Sincronize os modelos com o banco de dados
-sequelize.sync({ force: true }) // O uso de force: true irá forçar a criação das tabelas, cuidado com dados existentes
+  // Sincronização dos modelos com o banco de dados
+sequelize.sync({ force: true }) // "force: true" irá forçar a criação das tabelas
 .then(() => {
   console.log('Banco de dados sincronizado.');
 })
 .catch((err) => {
   console.error('Erro ao sincronizar o banco de dados:', err);
-});
+});*/
