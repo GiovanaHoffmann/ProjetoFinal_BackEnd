81308ab1d65ce32a37385bbb8e88d6924b7e73a3
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-12-10 00:51:31 -0300

    rota de instalação

--- Routes/installRoute.js
+++ Routes/installRoute.js
@@ -1,13 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/produto');
+const Usuario = require('../models/Usuario');
+const Funcionario = require('../models/funcionario');
+const Gerente = require('../models/gerente');
 
 router.get('/', async (req, res) => {
   try {
-    //await Product.sync({ force: true }); 
-    await Product.sync();
+    await Funcionario.bulkCreate([
+      { 
+        nome: 'Funcionario1',
+        email: 'funcionario1@example.com',
+        senha: 'senha123',
+        setor: 'DVD' 
+      },
+      { 
+        nome: 'Funcionario2',
+        email: 'funcionario2@example.com',
+        senha: 'senha456',
+        setor: 'CD' 
+      },
+      { 
+        nome: 'Funcionario3',
+        email: 'funcionario3@example.com',
+        senha: 'senha789',
+        setor: 'vinil' 
+      }
+    ]);
 
-    // Popula as tabelas/coleções com alguns registros
+     // Criação do gerente padrão
+     const novoUsuarioAdmin = await Usuario.create({
+      nome: 'Admin',
+      email: 'admin@example.com',
+      senha: 'admin123',
+      isAdmin: true,
+    });
+
+    const novoGerente = await Gerente.create({ UsuarioId: novoUsuarioAdmin.id });
+
+    await Gerente.create({});
+    await Product.sync();
     await Product.bulkCreate([
       { nome: 'Taylor Swift', artista: 'Taylor Swift', genero: 'Country', anoLancamento: 2006, preco: 10.99, tipo: 'cd' },
       { nome: 'Fearless', artista: 'Taylor Swift', genero: 'Country', anoLancamento: 2008, preco: 12.99, tipo: 'dvd' },
@@ -21,10 +53,10 @@ router.get('/', async (req, res) => {
       { nome: 'Midnights', artista: 'Taylor Swift', genero: 'pop', anoLancamento: 2022, preco: 12.99, tipo: 'dvd' },
     ]);
 
-    res.status(200).json({ message: 'Banco de dados instalado e populado com sucesso.' });
+    res.status(200).json({ message: 'Perfis criados e Banco de dados instalado e populado com sucesso.' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Erro ao popular o banco de dados.' });
+    res.status(500).json({ message: 'Erro ao popular o banco de dados e crias perfis.' });
   }
 });
 
