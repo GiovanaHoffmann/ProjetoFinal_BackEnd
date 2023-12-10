7853982ce86faf59725f27acd5e92d5bce8cd109
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-12-10 01:01:38 -0300

    ouitras alteraçoes

--- Routes/produtoRoutes.js
+++ Routes/produtoRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const ProductController = require('../control/produtoController');
 
-// Rotas para CRUD de produtos
+//Rotas CRUD de produtos
 router.get('/', ProductController.getAllProducts);
 router.get('/:id', ProductController.getProductById);
-router.post('/', ProductController.createProduct);
+/*router.post('/', ProductController.createProduct);
 router.put('/:id', ProductController.updateProduct);
-router.delete('/:id', ProductController.deleteProduct);
+router.delete('/:id', ProductController.deleteProduct);*/
 
 module.exports = router;
--- Routes/usuarioRoutes.js
+++ Routes/usuarioRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../control/usuarioController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-// Rotas para CRUD de usuários
+router.use(authMiddleware); //middleware de autenticação para todas as rotas de usuário
+
+//rota para obter perfil de usuário
+router.get('/profile', UserController.getUserProfile);
+//rotas para CRUD de usuários
 router.post('/register', UserController.registerUser);
 router.post('/login', UserController.loginUser);
 router.put('/:id', UserController.updateUser);
