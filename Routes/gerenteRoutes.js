702a07b6ddec3d5b1343b16998fb3ea775af7b5a
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-12-09 23:19:57 -0300

    atualização gerenteRoutes

--- Routes/gerenteRoutes.js
+++ Routes/gerenteRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const gerenteController = require('../control/gerenteController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const gerenteAuth = require('../middlewares/gerenteMiddleware');
 
-router.put('/estoque/:id', gerenteController.modificarEstoque);
-router.put('/usuarios/:id', gerenteController.gerenciarUsuarios);
+router.use(authMiddleware); // Middleware de autenticação para todas as rotas de gerente
+router.use(gerenteAuth); // Middleware de autorização para gerentes
+
+router.get('/produtos', gerenteController.listarProdutos);
+router.post('/produtos', gerenteController.criarProduto);
+router.put('/produtos/:id', gerenteController.atualizarProduto);
+router.delete('/produtos/:id', gerenteController.excluirProduto);
+router.post('/criar', gerenteController.criarGerente);
+
+router.post('/funcionarios', gerenteController.criarFuncionario);
+router.delete('/funcionarios/:id', gerenteController.excluirFuncionario);
 
 module.exports = router;
+
