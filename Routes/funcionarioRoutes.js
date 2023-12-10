3bb2b938e50a340b4fed160aa96b85512349878e
GiovanaHoffmann <88631348+GiovanaHoffmann@users.noreply.github.com>
2023-12-09 23:21:46 -0300

    adequação de funcionarioRoutes

--- Routes/funcionarioRoutes.js
+++ Routes/funcionarioRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const funcionarioController = require('../control/funcionarioController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const funcionarioAuth = require('../middlewares/funcionarioMiddleware');
+
+router.use(authMiddleware); // Middleware de autenticação para todas as rotas de funcionário
+router.use(funcionarioAuth); // Middleware de autorização para funcionários
 
 router.get('/estoque', funcionarioController.listarEstoque);
+router.post('/estoque', funcionarioController.criarEstoque);
 router.put('/estoque/:id', funcionarioController.atualizarEstoque);
-
+router.delete('/estoque/:id', funcionarioController.excluirEstoque);
 
 module.exports = router;
+
+
